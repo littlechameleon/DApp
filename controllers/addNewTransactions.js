b2805afb78fd2d41572c311f2a3f322b6a6d0b2b
littlechameleon <1187307315@qq.com>
2020-11-20 02:46:54 +0800

    修复controllers

--- controllers/addNewTransactions.js
+++ controllers/addNewTransactions.js
@@ -1,3 +1,6 @@
 module.exports = async ctx => {
-    
+    // let sender = ctx.request.body.sender
+    const newTransaction = ctx.request.body
+    testCoin.addNewTransaction(newTransaction)
+    ctx.body = `The transaction ${JSON.stringify(newTransaction)} is successfully added to the the blockchain.`
 }
--- controllers/mine.js
+++ controllers/mine.js
@@ -1,4 +1,16 @@
+const { Block } = require("../models/blockchain")
+
 module.exports = async ctx => {
-    
-    ctx.body = 'mine'
+    const latestBlockIndex = testCoin.chain.length
+    const newBlock = new Block(latestBlockIndex, new Date().toString())
+    newBlock.transactions = testCoin.currentTransactions
+    newBlock.transactions.unshift({
+        sender: '0',
+        recipient: nodeIdentifier,
+        amount: 50
+    })
+    testCoin.addBlock(newBlock)
+    testCoin.currentTransactions = []
+    ctx.body = `Mined new block ${JSON.stringify(newBlock, undefined, 2)}`
+    // ctx.body = 'mine'
 }
